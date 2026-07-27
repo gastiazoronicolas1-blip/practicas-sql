8351f68cda86fa972ab2fd8901430470829e9939
gastiazoronicolas1-blip <gastiazoronicolas1@gmail.com>
2026-07-27 19:08:05 -0300

    Add files via upload

---
+++ modulo2_unidad1_diseno.sql,.sql
@@ -0,0 +1,80 @@
+/* ============================================================
+
+                   DDL (Data Definition Language)
+
+   ============================================================ */ 
+
+ 
+
+ 
+
+--   Creamos una tabla, la llamaremos DimCampanias
+CREATE TABLE dbo.Campana(
+id_Campana INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+Campaing VARCHAR (50))
+ 
+
+ 
+
+--  Agregamos una columna
+ALTER TABLE id_Campana
+ADD Descripcion VARCHAR(50)
+ 
+
+ 
+
+ 
+
+ 
+
+/*
+
+Hacemos una consulta para ver si cumplimos la consigna
+
+*/
+
+ 
+
+ 
+
+ 
+
+--   Eliminamos una columna
+
+ 
+
+ 
+
+ 
+
+ 
+
+/* ============================================================
+
+                   DML (Data Manipulation Language)
+
+   ============================================================ */ 
+
+ 
+
+--   Ingresar registros
+
+ 
+
+ 
+
+ 
+
+ 
+
+--   Modificar registros
+
+ 
+
+ 
+
+ 
+
+ 
+
+--   Eliminar un registro puntual
